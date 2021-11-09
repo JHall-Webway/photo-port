4e76531bb6381e0de4e732a60af1391b4034d1c0
JHallUofA <83625694+JHallUofA@users.noreply.github.com>
2021-11-09 13:06:09 -0800

    add form state

---
+++ src/components/Contact/__tests__/index.test.js
@@ -0,0 +1,16 @@
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import ContactForm from '..';
+
+afterEach(cleanup);
+
+describe('Contact form is rendering', () => {
+    it('renders', () => {
+        render(<ContactForm />);
+    });
+    it('matches snapshot', () => {
+        const { asFragment } = render(<ContactForm />);
+        expect(asFragment()).toMatchSnapshot();
+    });
+    
+});
